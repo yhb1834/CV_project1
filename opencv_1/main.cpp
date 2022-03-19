2cb1edf92420b1f0ab60cae05839af50d5233af0
yhb1834 <yhb1834@hanmail.net>
2022-03-19 14:13:32 +0900

    Initial Commit

---
+++ opencv_1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opencv_1
+//
+//  Created by 이한별 on 3/19/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
